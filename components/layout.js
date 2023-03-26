93519a40f128ddf30e3586b2a0d0c614af6b2772
RamiBouk <ramiboukaroura@gmail.com>
2023-03-26 16:19:20 +0100

    init

---
+++ components/layout.js
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from '../styles/utils.module.css';
+import styles from'./layout.module.css';
+import utilStyles from'../styles/utils.module.css';
+
+const name='Rami B.'
+export const siteTitle='next.js sample website'
+
+export default function Layout({children,home}){
+	return (
+		<div  className={styles.container}>
+			<Head>
+				<link rel="icon" href="/favicon.ico"/>
+				<meta 
+					name="description"
+					content="learn how to build personal website using next.js"/>
+				<meta
+					property="og:image"
+					content={`https://og-image.vercel.app/${encodeURI(
+						siteTitle,
+					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+				/>
+				<meta name="og:title" content={siteTitle} />
+				<meta name="twitter:card" content="summary_large_image" />
+			</Head>
+			<header className={styles.header}>
+				{home?(<>
+					<Image
+						priority
+						src='/profile.jpg'
+						className={utilStyles.borderCircle}
+						height={144}
+						width={144}
+						alt="error"
+					/>
+					<h1 className={utilStyles.heading2Xl}>{name}</h1>
+				</>
+				):(
+					<>
+						<Link href="/">
+							<Image
+								priority
+								src='/images/profile.jpg'
+								className={utilStyles.borderCircle}
+								height={144}
+								width={144}
+								alt="error"
+							/>
+						</Link>
+						<h2 className={utilStyles.headingLg}>
+							<Link href="/" className={utilStyles.colorInherit}>
+								{name}
+							</Link>
+						</h2>
+					</>
+
+				)}
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className={styles.backToHome}>
+					<Link href="/">← Back to home</Link>
+				</div>
+			)}
+		</div>
+	);
+}
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Computer science student with a strong work ethic, a passion for computer science and a solid foundation in math. 
+          Experienced in developing and testing cross-platform mobile applications. 
+        Worked for an international software development company. 
+        Fluent in three languages and currently learning German.</p>
+      </section>
+    </Layout>
+    </>
+  );
+}
