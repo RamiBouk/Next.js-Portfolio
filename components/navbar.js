cfdabba6476d3c23bb60543a269a627e91253ee9
RamiBouk <ramiboukaroura@gmail.com>
2023-03-28 14:16:04 +0100

    made the navbar fit the theme

--- components/navbar.js
+++ components/navbar.js
@@ -2,83 +2,82 @@ import {useState} from "react";
 import Link from "next/link";
 
 export default function NavBar(){
-    const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-	return  <nav className="w-full bg-gray-800 shadow">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-          <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <a href="#">
-                <h2 className="text-2xl text-white font-bold">NEXT JS</h2>
-              </a>
-              <div className="md:hidden">
-                <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'block' : 'hidden'
-              }`}
+  return  <nav className="w-full bg-black shadow">
+    <div className="justify-between px-4 mx-auto lg:max-w-21xl md:items-center md:flex md:px-8">
+      <div>
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <h2 className="text-2xl text-white font-bold">
+            <Link href="#"className="text-white">
+              NEXT JS          </Link></h2>
+
+          <div className="md:hidden">
+            <button
+              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}
             >
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-white">
-                  <Link href="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/blogs">
-                   Blogs
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/about">
-                    About US
-                  </Link>
-                </li>
-                <li className="text-white">
-                  <Link href="/contact">
-                    Contact US
-                  </Link>
-                </li>
-              </ul>
-            </div>
+              {navbar ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-white"treesitter nvim
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
         </div>
-      </nav>;
+      </div>
+      <div>
+        <div
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? 'block' : 'hidden'
+              }`}
+        >
+          <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            <li className="text-white">
+              <Link href="/"className="text-white">
+                Home
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link href="/blogs"className="text-white">
+                Blogs
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link href="/about" className="text-white">
+                About US
+              </Link>
+            </li>
+            <li className="text-white">
+              <Link href="/contact"className="text-white">
+                Contact US
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>;
 }
