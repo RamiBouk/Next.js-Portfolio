37e7efd4baad9c1849d84891a38145c23262bc87
RamiBouk <ramiboukaroura@gmail.com>
2023-03-29 08:40:52 +0100

    testig if this fix the pictures

--- components/layout.js
+++ components/layout.js
@@ -29,7 +29,7 @@ export default function Layout({children,home}){
 				<>
 					<Image
 						priority
-						src="/profile.jpg"
+						src={profilePic}
 						className={utilStyles.borderCircle}
 						height={144}
 						width={144}
